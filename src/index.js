f6e352e6137f20db3b9cbe8aa4ad6c9a3aad2291
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-08-17 22:14:14 +0900

    기본셋팅(3)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./common/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </Provider>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
