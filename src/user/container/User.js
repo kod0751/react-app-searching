10fbe44831d09f7b115ad3a08a3bae3028bdf2ff
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-08-23 00:18:01 +0900

    사용자페이지 UI

--- src/user/container/User.js
+++ src/user/container/User.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Col, PageHeader, Row } from "antd";
+import { Col, Descriptions, PageHeader, Row, Typography } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../state";
@@ -15,12 +15,30 @@ export default function User() {
     dispatch(actions.fetchUser(name));
   }, [name]);
 
+  const isFetched = true;
+
   return (
     <>
       <Row justify="center">
         <Col xs={24} md={20} lg={14}>
           <PageHeader onBack={() => navigate(-1)} title="사용자 정보">
-            {user?.name}
+            {user && (
+              <Descriptions layout="vertical" bordered column={1}>
+                <Descriptions.Item label="이름">
+                  <Typography.Text>{user.name}</Typography.Text>
+                </Descriptions.Item>
+                <Descriptions.Item label="소속">
+                  {user.department}
+                </Descriptions.Item>
+                <Descriptions.Item label="태그">{user.tag}</Descriptions.Item>
+                <Descriptions.Item label="수정 내역">
+                  수정 내역
+                </Descriptions.Item>
+              </Descriptions>
+            )}
+            {!user && isFetched && (
+              <Typography.Text>존재하지 않는 사용자 입니다.</Typography.Text>
+            )}
           </PageHeader>
         </Col>
       </Row>
