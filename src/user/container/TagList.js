2e0bf1e33ed8805521bb820b0d14e334fa5a8923
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-08-26 09:46:55 +0900

    사용자 정보 수정 - 태그정보

---
+++ src/user/container/TagList.js
@@ -0,0 +1,32 @@
+import { Tag } from "antd";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "../state";
+
+export default function TagList() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+  const tags = user?.tag ? user.tag.split(",").map((item) => item.trim()) : [];
+
+  function onDelete(tag) {
+    const newTag = tags.filter((item) => item !== tag).join(", ");
+    dispatch(
+      actions.fetchUpdateUser({
+        user,
+        key: "tag",
+        value: newTag,
+        fetchKey: "tag",
+      })
+    );
+  }
+
+  return (
+    <>
+      {tags.map((item) => (
+        <Tag key={item} closable onClose={() => onDelete(item)}>
+          {item}
+        </Tag>
+      ))}
+    </>
+  );
+}
