b119e33644c22300cfa4ff34ff81bdf07f4d72f9
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-08-26 10:10:12 +0900

    사용자 정보 수정 - 태그정보(2)
    onAdd - 태그정보 추가

--- src/user/container/TagList.js
+++ src/user/container/TagList.js
@@ -1,5 +1,6 @@
-import { Tag } from "antd";
-import React from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { Input, Tag } from "antd";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../state";
 
@@ -7,6 +8,15 @@ export default function TagList() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
   const tags = user?.tag ? user.tag.split(",").map((item) => item.trim()) : [];
+
+  const [isAdd, setIsAdd] = useState(false);
+  const [tempTag, setTempTag] = useState("");
+  function onAdd() {
+    setIsAdd(true);
+    setTempTag("");
+  }
+
+  function onSave() {}
 
   function onDelete(tag) {
     const newTag = tags.filter((item) => item !== tag).join(", ");
@@ -27,6 +37,23 @@ export default function TagList() {
           {item}
         </Tag>
       ))}
+      {!isAdd && (
+        <Tag onClick={onAdd}>
+          <PlusOutlined /> New Tag
+        </Tag>
+      )}
+      {isAdd && (
+        <Input
+          autoFocus
+          type="text"
+          size="small"
+          style={{ width: 100 }}
+          value={tempTag}
+          onChange={(e) => setTempTag(e.target.value)}
+          onBlur={() => setIsAdd(false)}
+          onPressEnter={onSave}
+        />
+      )}
     </>
   );
 }
